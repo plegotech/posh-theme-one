9a974930b1395ab5b385af63bc64641c1ff86362
Mashhood <mashood@plego.com>
2022-02-16 04:31:19 +0500

    vue-carousel fix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios';
 
+import 'vue3-carousel/dist/carousel.css';
+
 axios.defaults.baseURL="https://posh-marketplace.plego.pro/api/";
 //Vue.prototype.$http = axios;
 createApp(App).use(router, axios).mount('#app')
